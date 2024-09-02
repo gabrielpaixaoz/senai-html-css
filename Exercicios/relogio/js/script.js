3ef169793d161510fb346467442fd9f8097b6683
Your Name <you@example.com>
2024-09-02 16:19:07 -0300

    bora

--- Exercicios/relogio/js/script.js
+++ Exercicios/relogio/js/script.js
@@ -8,6 +8,24 @@ setInterval(relogio,1000)
 function relogio(){
     let hoje = new Date()
     let h = hoje.getHours()
-}
+    let m = hoje.getMinutes()
+    let s = hoje.getSeconds()
 
+    
+    if(h < 10){
+        h = "0" + h
+    }
+    if(m < 10){
+        m = "0" + m
+    }
+    if(s < 10){
+        s = "0" + s
+        
+    }
+    horas.textContent = h
+    minutos.textContent = m
+    segundos.textContent = s
+
+    
+}
 
